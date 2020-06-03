c27b892fece7b9d2fff00f23694d67c314b9c540
Tommy <taguilu01@gmail.com>
2020-06-02 19:44:23 -0700

    adding black

--- blogging/urls.py
+++ blogging/urls.py
@@ -3,9 +3,12 @@ from blogging.views import list_view, detail_view, PostViewSet
 from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
+
 urlpatterns = [
     path("", list_view, name="blog_index"),
-    path("/api", PostViewSet, name="post_views"),
+    path("api/", include(router.urls)),
     path("posts/<int:post_id>/", detail_view, name="blog_detail"),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
